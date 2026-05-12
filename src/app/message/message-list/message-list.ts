538d9aa798fbdef8850d297ad889f4b31de2e3bc
Simon <simon@Simons-MacBook-Air.local>
2026-05-12 19:44:15 +0200

    fertig glaub

--- src/app/message/message-list/message-list.ts
+++ src/app/message/message-list/message-list.ts
@@ -2,11 +2,12 @@ import { Component, inject, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from "@angular/router";
 import { LoginApi } from '../../api.service';
+import { FormsModule } from '@angular/forms';
 
 
 @Component({
   selector: 'app-message-list',
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, FormsModule],
   templateUrl: './message-list.html',
   styleUrl: './message-list.css',
 })
@@ -19,7 +20,8 @@ export class MessageList {
   readonly errorMes = signal('');
 
   readonly contacts = signal<any[]>([]);
-  readonly receiverId = signal<any | null>(null);
+  receiverId: string | null = null;
+  newMessage = '';
   //hardcoded
   //receiverId = '1344';
 
@@ -35,12 +37,13 @@ export class MessageList {
 
   }
 
-  loadConversation(user: any) {
-    console.log(user);
+  loadConversation(otherUser: any) {
+    console.log(otherUser);
     this.isLoading.set(true);
     const token = this.loginApi.token();
     const userId = this.loginApi.userId();
-    fetch(`http://webp-ilv-backend.cs.technikum-wien.at/messenger/get_conversation.php?token=${token}&user1_id=${userId}&user2_id=${user.id}`)
+    this.receiverId = otherUser.id;
+    fetch(`http://webp-ilv-backend.cs.technikum-wien.at/messenger/get_conversation.php?token=${token}&user1_id=${userId}&user2_id=${this.receiverId}`)
       .then(result => result.json())
       .then(data => {
 
@@ -53,6 +56,50 @@ export class MessageList {
       .finally(() => this.isLoading.set(false));
       console.log("durch");
   }
+
+  sendMessage(): void {
+  console.log("sendMessage wurde ausgeführt!");
+  const token = this.loginApi.token();
+  const senderId = this.loginApi.userId();
+
+  if (!token || !senderId || !this.receiverId) {
+    console.log(senderId);
+    console.log(token);
+    console.log(this.receiverId);
+    return;
+  }
+
+  const formData = new FormData();
+
+  formData.append('token', token);
+  formData.append('sender_id', senderId);
+  formData.append('receiver_id', this.receiverId);
+  formData.append('message', this.newMessage.trim());
+
+  fetch(
+    'http://webp-ilv-backend.cs.technikum-wien.at/messenger/send_message.php',
+    {
+      method: 'POST',
+      body: formData
+    }
+  )
+  .then(res => res.json())
+  .then(() => {
+
+    // sofort lokal hinzufügen
+    this.messages.update(msgs => [
+      ...msgs,
+      {
+        sender_id: senderId,
+        message: this.newMessage
+      }
+    ]);
+
+    this.newMessage = '';
+    console.log("message sent");
+
+  });
+}
 
 }
 
